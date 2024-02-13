c40b6739d5db265591afe14e8ea7cb1265d99113
Greg Wilding <gregory.wilding@ucalgary.ca>
2024-02-13 13:28:36 -0700

    Commenting

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this will create an entry "Counter" at the top of the create menu to add
+// an instance of the Counter ScriptableObject.
 [CreateAssetMenu]
 public class Counter : ScriptableObject
 {
